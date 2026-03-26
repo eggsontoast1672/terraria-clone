7c8b7edc53562be7af35c2a40e64f31be4d68f0d
Paul Zupan <paul.zupan.13@gmail.com>
2026-03-26 13:06:12 -0700

    Add assert code

---
+++ src/platform/asserts.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+void assertFuncProduction(const char *expression, const char *file_name, unsigned line_number, const char *comment = "---");
+void assertFuncInternal(const char *expression, const char *file_name, unsigned line_number, const char *comment = "---");
+
+#if DEVELOPLEMT_BUILD
+#define permaAssert(expression) \
+    (void)((!!(expression)) ||  \
+           (assertFuncInternal(#expression, __FILE__, (unsigned)(__LINE__)), 0))
+#define permaAssertComment(expression, comment) \
+    (void)((!!(expression)) ||                  \
+           (assertFuncInternal(#expression, __FILE__, (unsigned)(__LINE__), comment), 1))
+#else
+#define permaAssert(expression) \
+    (void)((!!(expression)) ||  \
+           (assertFuncProduction(#expression, __FILE__, (unsigned)(__LINE__)), 0))
+#define permaAssertComment(expression, comment) \
+    (void)((!!(expression)) ||                  \
+           (assertFuncProduction(#expression, __FILE__, (unsigned)(__LINE__), comment), 1))
+#endif
+
+#if DEVELOPLEMT_BUILD
+#define permaAssertDevelopement permaAssert
+#define permaAssertCommentDevelopement permaAssertComment
+#else
+#define permaAssertDevelopement
+#define permaAssertCommentDevelopement
+#endif
